16e859efdc25cb8f952e2600e5c18e743f940946
Michael Rudow <mrudow@ftwdev.com>
2013-07-24 09:12:41 -0400

    cleaned up javascript of unnecessary code

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
   $('.best_in_place').best_in_place();  
-  $('#edit_form').hide();
-  $('button').on('click', function(){
-    $('#edit_form').slideToggle();
-  });
-});
+ });
 
