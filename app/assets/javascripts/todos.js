d715eaaff9c4c403c895f4a879472de819cd9488
Michael Rudow <mrudow@ftwdev.com>
2013-07-26 12:46:06 -0400

    clicking on the notes button now toggles the note for that item in the todos list. It requires the button html tag to be directly before the tag for the note in the code

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('.notes').hide();
   $('.best_in_place').best_in_place();
   $('button').on('click', function(){
-    $('.notes').slideToggle();
+    $(this).next('.notes').slideToggle();
+    //$('.notes').slideToggle();
   });
  });
 
